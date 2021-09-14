efbc57078f000d50110ac9e7b573dfaea58db597
Muhammad Azri <lenovorosak@gmail.com>
2021-09-14 22:32:51 +0800

    add image path and fix endpoint path

--- pages/eom.js
+++ pages/eom.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 const EOM = ({employee}) => {
+  console.log(employee);
   return (
     <div className='page-container'>
       <div className=''>
         <h1>Employee of the Month</h1>
+
       </div>
     </div>
   )
 }
 
 export const getServerSideProps = async pageContext => {
-  const apiResponse = await fetch('https://my-json-server.typicode.com/AzriZzz/next-news-app');
+  const apiResponse = await fetch('https://my-json-server.typicode.com/AzriZzz/next-news-app/employeeOfTheMonth');
   const employee = await apiResponse.json();
 
   return {
