2340ef52f98a01460280960b52cb727badc834e1
Muhammad Azri <lenovorosak@gmail.com>
2021-09-15 00:09:47 +0800

    create new page and integrate the news api

--- pages/eom.js
+++ pages/eom.js
@@ -1,27 +1,62 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/EOM.module.css";
+import { Navbar } from "../components/Navbar";
+import Head from 'next/head'
 
-const EOM = ({employee}) => {
-  console.log(employee);
+const EOM = ({ employee }) => {
+
   return (
-    <div className='page-container'>
-      <div className=''>
-        <h1>Employee of the Month</h1>
+    <div>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name="description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
 
+        <meta property="og:image" content={employee.image} />
+        <meta property="og:title" content="Employee Of The Month" />
+        <meta
+          property="og:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property="twitter:image" content={employee.image} />
+        <meta property="twitter:title" content="Employee Of The Month" />
+        <meta
+          property="twitter:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+    <div className="page-container">
+      <Navbar />
+      <div className={styles.main}>
+        <h1>Employee of the Month</h1>
+        <div className={styles.employeeOfTheMonth}>
+          <h3>{employee.name}</h3>
+          <h4>{employee.position}</h4>
+          <img src={employee.image} alt="Profile Image" />
+          <p>{employee.description}</p>
+        </div>
       </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export const getServerSideProps = async pageContext => {
-  const apiResponse = await fetch('https://my-json-server.typicode.com/AzriZzz/next-news-app/employeeOfTheMonth');
-  const employee = await apiResponse.json();
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/AzriZzz/next-news-app/employeeOfTheMonth"
+  );
+  let employee = await apiResponse.json();
+  employee.image = `${process.env.HOST}/images/profile_img.png`;
 
   return {
     props: {
-      employee
-    }
-  }
+      employee,
+    },
+  };
+};
 
-}
-
-export default EOM
+export default EOM;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Navbar } from "../components/Navbar";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
-  )
+    <div>
+      <Head>
+        <title>Home </title>
+        <meta
+          name="description"
+          content="News app build using Next.js"
+        />
+
+        <meta property="og:image" content={`${process.env.HOST}/images/home.png`} />
+        <meta property="og:title" content="Home" />
+        <meta
+          property="og:description"
+          content="News app build using Next.js"
+        />
+
+        <meta property="twitter:image" content={`${process.env.HOST}/images/home.png`} />
+        <meta property="twitter:title" content="Home" />
+        <meta
+          property="twitter:description"
+          content="News app build using Next.js"
+        />
+      </Head>
+      <div className="page-container">
+        <Navbar />
+        <div className={styles.main}>
+          <h1>Next.js News App</h1>
+          <h3>Your one stop shop for the latest news articles on Sport</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
